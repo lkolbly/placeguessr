90aaf9a171ae4979f47f821f54ac23569ddcad39
Lane Kolbly <lane@rscheme.org>
2020-11-23 17:37:38 -0600

    Add BoundaryFilter to generate_places

--- generate_places/src/main.rs
+++ generate_places/src/main.rs
@@ -6,7 +6,7 @@ use simple_process_stats::ProcessStats;
 use std::collections::{HashMap, HashSet};
 use std::io::prelude::*;
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 struct Location {
     latitude: f32,
     longitude: f32,
@@ -233,6 +233,9 @@ struct RoadExtractor {
     node_ids: HashSet<i64>,
     nodes: HashMap<i64, Location>,
     roads: Vec<Vec<i64>>,
+
+    /// This is an optimization, so the output can be used many times
+    points: Vec<Location>,
 }
 
 impl RoadExtractor {
@@ -241,6 +244,7 @@ impl RoadExtractor {
             node_ids: HashSet::new(),
             nodes: HashMap::new(),
             roads: vec![],
+            points: vec![],
         }
     }
 
@@ -289,7 +293,7 @@ impl RoadExtractor {
         }
     }
 
-    fn export(&self, mut writer: impl PointWriter) {
+    fn compute_points(&mut self) {
         // First, compute the total length of all roads
         info!("Computing road lengths for {} roads...", self.roads.len());
         info!("Have {} nodes as parts of roads", self.node_ids.len());
@@ -393,10 +397,320 @@ impl RoadExtractor {
                     offset, road_lengths[road_idx], alpha, pnt
                 );
             } else {
-                writer.write(&pnt);
+                //writer.write(&pnt);
+                self.points.push(pnt);
             }
         }
         info!("Finished exporting road points");
+    }
+
+    fn export(&self, mut writer: impl PointWriter) {
+        for point in self.points.iter() {
+            writer.write(&point);
+        }
+    }
+}
+
+struct DebugPointTee<T: PointWriter> {
+    writer: T,
+    file: std::fs::File,
+}
+
+impl<T: PointWriter> DebugPointTee<T> {
+    fn new(path: &str, writer: T) -> DebugPointTee<T> {
+        DebugPointTee {
+            writer: writer,
+            file: std::fs::File::create(path).unwrap(),
+        }
+    }
+}
+
+impl<T: PointWriter> PointWriter for DebugPointTee<T> {
+    fn write(&mut self, location: &Location) {
+        self.writer.write(location);
+        self.file
+            .write_all(format!("{},{}\n", location.latitude, location.longitude,).as_bytes())
+            .unwrap();
+    }
+}
+
+struct BoundaryFilter {
+    edges: Vec<(Location, Location)>,
+}
+
+fn lerp(x1: f32, x2: f32, y1: f32, y2: f32, x: f32) -> f32 {
+    let a = (x - x1) / (x2 - x1);
+    (y2 - y1) * a + y1
+}
+
+impl BoundaryFilter {
+    fn contains(&self, location: &Location) -> bool {
+        let mut num_crossings = 0;
+        for (a, b) in self.edges.iter() {
+            if a.longitude <= location.longitude && b.longitude <= location.longitude {
+                // Line is too far West to matter
+            } else if a.longitude >= location.longitude && b.longitude >= location.longitude {
+                // Line is too far East to matter
+            } else if a.latitude >= location.latitude && b.latitude >= location.latitude {
+                // Line is too far North to matter
+            } else if a.latitude <= location.latitude && b.latitude <= location.latitude {
+                num_crossings += 1;
+            } else {
+                let latitude = lerp(
+                    a.longitude,
+                    b.longitude,
+                    a.latitude,
+                    b.latitude,
+                    location.longitude,
+                );
+                if latitude <= location.latitude {
+                    num_crossings += 1;
+                }
+            }
+        }
+        num_crossings % 2 == 1
+    }
+}
+
+struct BoundaryFilterWriter<T: PointWriter> {
+    writer: T,
+
+    /// This should be sorted by longitude of the first element
+    //edges: Vec<(Location, Location)>,
+    filters: Vec<BoundaryFilter>,
+}
+
+impl<T: PointWriter> BoundaryFilterWriter<T> {
+    fn new(filters: Vec<BoundaryFilter>, writer: T) -> BoundaryFilterWriter<T> {
+        BoundaryFilterWriter { writer, filters }
+    }
+}
+
+impl<T: PointWriter> PointWriter for BoundaryFilterWriter<T> {
+    fn write(&mut self, location: &Location) {
+        /*let mut num_crossings = 0;
+        for (a, b) in self.edges.iter() {
+            if a.longitude <= location.longitude && b.longitude <= location.longitude {
+                // Line is too far West to matter
+            } else if a.longitude >= location.longitude && b.longitude >= location.longitude {
+                // Line is too far East to matter
+            } else if a.latitude >= location.latitude && b.latitude >= location.latitude {
+                // Line is too far North to matter
+            } else if a.latitude <= location.latitude && b.latitude <= location.latitude {
+                num_crossings += 1;
+            } else {
+                let latitude = lerp(
+                    a.longitude,
+                    b.longitude,
+                    a.latitude,
+                    b.latitude,
+                    location.longitude,
+                );
+                if latitude <= location.latitude {
+                    num_crossings += 1;
+                }
+            }
+        }
+        if num_crossings % 2 == 1 {*/
+        if self
+            .filters
+            .iter()
+            .fold(false, |acc, x| acc | x.contains(location))
+        {
+            self.writer.write(location);
+        }
+    }
+}
+
+type RelId = i64;
+type WayId = i64;
+type NodeId = i64;
+
+struct BoundaryFinder {
+    boundaries: HashMap<String, RelId>,
+    boundary_ways: HashMap<RelId, Vec<WayId>>,
+    way_ids: HashSet<WayId>,
+    ways: HashMap<WayId, Vec<NodeId>>,
+    node_ids: HashSet<NodeId>,
+    nodes: HashMap<NodeId, Location>,
+}
+
+impl BoundaryFinder {
+    fn new() -> BoundaryFinder {
+        BoundaryFinder {
+            boundaries: HashMap::new(),
+            boundary_ways: HashMap::new(),
+            way_ids: HashSet::new(),
+            ways: HashMap::new(),
+            node_ids: HashSet::new(),
+            nodes: HashMap::new(),
+        }
+    }
+
+    fn dump_to_file(&self, relid: i64, filepath: &str) {
+        for (k, ways) in self.boundary_ways.iter() {
+            info!("Boundary {} has {} ways", k, ways.len());
+        }
+
+        let mut edges = vec![];
+        for way_id in self.boundary_ways.get(&relid).unwrap().iter() {
+            for nodes in self.ways.get(way_id).unwrap().windows(2) {
+                let node_a = self.nodes.get(&nodes[0]).unwrap();
+                let node_b = self.nodes.get(&nodes[1]).unwrap();
+                edges.push(((*node_a).clone(), (*node_b).clone()));
+            }
+        }
+        let mut file = std::fs::File::create("geo.csv").unwrap();
+        for (a, b) in edges.iter() {
+            file.write_all(
+                format!(
+                    "{},{},{},{}\n",
+                    a.latitude, a.longitude, b.latitude, b.longitude
+                )
+                .as_bytes(),
+            )
+            .unwrap();
+        }
+    }
+
+    fn filter(&self, relid: i64) -> BoundaryFilter {
+        if !self.boundary_ways.contains_key(&relid) {
+            error!("Could not find boundary relation ID {}!", relid);
+            return BoundaryFilter { edges: vec![] };
+        }
+        let mut edges = vec![];
+        for way_id in self.boundary_ways.get(&relid).unwrap().iter() {
+            if !self.ways.contains_key(way_id) {
+                error!(
+                    "Could not find way {} inside boundary relation {}",
+                    way_id, relid
+                );
+                return BoundaryFilter { edges: vec![] };
+            }
+            for nodes in self.ways.get(way_id).unwrap().windows(2) {
+                let node_a = self.nodes.get(&nodes[0]).unwrap();
+                let node_b = self.nodes.get(&nodes[1]).unwrap();
+                edges.push(((*node_a).clone(), (*node_b).clone()));
+            }
+        }
+        edges.sort_by(|(a, _), (b, _)| a.longitude.partial_cmp(&b.longitude).unwrap());
+        BoundaryFilter { edges }
+    }
+
+    /// Find all "ways" which are a part of a border
+    fn find_boundaries(&mut self, element: &Element) {
+        match element {
+            Element::DenseNode(_) | Element::Node(_) => {}
+            Element::Way(way) => {
+                /*for (k, v) in way.tags() {
+                    if k == "boundary" && v == "administrative" {
+                        self.boundary_ways.insert(way.id());
+                    }
+                }*/
+            }
+            Element::Relation(rel) => {
+                let mut name = String::new();
+                for (k, v) in rel.tags() {
+                    if k == "name" {
+                        name = v.to_string();
+                        break;
+                    }
+                }
+                for (k, v) in rel.tags() {
+                    if k == "boundary" && v == "administrative" {
+                        let mut way_ids = vec![];
+                        for member in rel.members() {
+                            match member.role() {
+                                Err(_) => {
+                                    panic!(format!(
+                                        "Found unknown member role {}!",
+                                        member.role_sid
+                                    ));
+                                }
+                                Ok("outer") | Ok("inner") => {
+                                    if member.member_type
+                                        == osmpbf::elements::RelMemberType::Relation
+                                    {
+                                        panic!("Found relation as outer/inner member of relation! Where is this allowed?");
+                                    }
+                                    //self.boundary_ways.push(member.member_id);
+                                    way_ids.push(member.member_id);
+                                    self.way_ids.insert(member.member_id);
+                                }
+                                Ok(_) => {
+                                    // Ignore
+                                }
+                            }
+                        }
+                        self.boundaries.insert(name, rel.id());
+                        self.boundary_ways.insert(rel.id(), way_ids);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    /// Enumerate all nodes which are a part of a border way
+    fn find_node_ids(&mut self, element: &Element) {
+        match element {
+            Element::DenseNode(node) => {
+                //
+            }
+            Element::Node(node) => {
+                //
+            }
+            Element::Way(way) => {
+                if !self.way_ids.contains(&way.id()) {
+                    return;
+                }
+                // Check that this way has the boundary tag
+                /*let mut has_boundary_tag = false;
+                for (k, v) in way.tags() {
+                    if k == "boundary" && v == "administrative" {
+                        has_boundary_tag = true;
+                        break;
+                    }
+                }
+                if !has_boundary_tag {
+                    log::warn!("Found way {} without boundary tag!", way.id());
+                }*/
+
+                // Extract all the node IDs
+                let node_ids: Vec<i64> = way.refs().collect();
+                for node_id in node_ids.iter() {
+                    self.node_ids.insert(*node_id);
+                }
+                self.ways.insert(way.id(), node_ids);
+            }
+            Element::Relation(rel) => {
+                // Ignore
+            }
+        }
+    }
+
+    /// Extract the lat/lng of every border node
+    fn find_nodes(&mut self, element: &Element) {
+        match element {
+            Element::DenseNode(node) => {
+                if self.node_ids.contains(&node.id) {
+                    self.nodes
+                        .insert(node.id, Location::new(node.lat(), node.lon()));
+                }
+            }
+            Element::Node(node) => {
+                if self.node_ids.contains(&node.id()) {
+                    self.nodes
+                        .insert(node.id(), Location::new(node.lat(), node.lon()));
+                }
+            }
+            Element::Way(way) => {
+                // Ignore
+            }
+            Element::Relation(rel) => {
+                // Ignore
+            }
+        }
     }
 }
 
@@ -436,10 +750,11 @@ impl Counter {
     }
 }
 
-fn do_pass<F>(mut cb: F)
+fn do_pass<F>(/*path: &str, nnodes: u64,*/ mut cb: F)
 where
     F: FnMut(&Element),
 {
+    //let reader = ElementReader::from_path(path).unwrap();
     let (reader, nnodes) = (
         ElementReader::from_path("/home/lane/Downloads/planet-190812.osm.pbf").unwrap(),
         6461362092u64,
@@ -448,7 +763,10 @@ where
         ElementReader::from_path("/home/lane/Downloads/texas-latest.osm.pbf").unwrap(),
         57964233u64,
     );*/
-    //let (reader, nnodes) = (ElementReader::from_path("/home/lane/Downloads/delaware-latest.osm.pbf").unwrap(), 1703775u64);
+    /*let (reader, nnodes) = (
+        ElementReader::from_path("/home/lane/Downloads/delaware-latest.osm.pbf").unwrap(),
+        1703775u64,
+    );*/
 
     // Increment the counter by one for each way.
     let bar = ProgressBar::new(nnodes);
@@ -481,8 +799,10 @@ async fn main() {
 
     // TODO: Check that there aren't variances across McDonald's (e.g. capitalization, having wikidata numbers)
     let mut mcdonalds = KvNodeExtractor::new("brand", Some("McDonald's"));
+    let mut walmart = KvNodeExtractor::new("brand", Some("Walmart"));
     let mut counter = Counter::new();
     let mut roads = RoadExtractor::new();
+    let mut geographic_filter = BoundaryFinder::new();
 
     // Increment the counter by one for each way.
     /*let mut bar = ProgressBar::new(57964233u64);
@@ -509,8 +829,10 @@ async fn main() {
 
     do_pass(|element| {
         mcdonalds.process(&element);
+        walmart.process(&element);
         counter.process(&element);
         roads.first_pass(&element);
+        geographic_filter.find_boundaries(&element);
     });
 
     info!(
@@ -518,12 +840,21 @@ async fn main() {
         mcdonalds.nodes.len(),
         mcdonalds.node_ids.len()
     );
+    info!(
+        "Number of Walmarts: {} nodes + {} ways",
+        walmart.nodes.len(),
+        walmart.node_ids.len()
+    );
     info!("Number of ways: {}", counter.ways);
     info!(
         "Number of nodes: {} of which {} were dense",
         counter.nodes, counter.dense_nodes
     );
     info!("Number of relations: {}", counter.relations);
+    info!(
+        "Number of boundary relations: {}",
+        geographic_filter.boundary_ways.len()
+    );
 
     info!(
         "Memory usage after first pass: {}KB",
@@ -532,7 +863,9 @@ async fn main() {
 
     do_pass(|element| {
         mcdonalds.second_pass(&element);
+        walmart.second_pass(&element);
         roads.second_pass(&element);
+        geographic_filter.find_node_ids(&element);
     });
 
     info!(
@@ -540,6 +873,74 @@ async fn main() {
         ProcessStats::get().await.unwrap().memory_usage_bytes / 1000
     );
 
+    do_pass(|element| {
+        geographic_filter.find_nodes(&element);
+    });
+
+    info!(
+        "Memory usage after third pass: {}KB",
+        ProcessStats::get().await.unwrap().memory_usage_bytes / 1000
+    );
+
+    info!(
+        "Boundary relations/ways/nodes: {}/{}/{}",
+        geographic_filter.boundaries.len(),
+        geographic_filter.way_ids.len(),
+        geographic_filter.node_ids.len()
+    );
+
+    roads.compute_points();
+
     mcdonalds.export(FilePointWriter::new("mcdonalds.dat"));
+    walmart.export(FilePointWriter::new("walmart.dat"));
     roads.export(FilePointWriter::new("roads.dat"));
+    geographic_filter.dump_to_file(117177, "tmp.csv");
+    /*roads.export(geographic_filter.filter(
+        117177,
+        DebugPointTee::new("pnts.csv", FilePointWriter::new("roads-cheswold.dat")),
+    ));*/
+    let g = geographic_filter;
+    roads.export(BoundaryFilterWriter::new(
+        vec![g.filter(117177)],
+        FilePointWriter::new("roads-cheswold.dat"),
+    ));
+    roads.export(BoundaryFilterWriter::new(
+        vec![g.filter(114690)],
+        FilePointWriter::new("roads-texas.dat"),
+    ));
+    roads.export(BoundaryFilterWriter::new(
+        vec![g.filter(148838)],
+        FilePointWriter::new("roads-us.dat"),
+    ));
+    roads.export(BoundaryFilterWriter::new(
+        vec![
+            g.filter(16239),   // Austria
+            g.filter(52411),   // Belgium
+            g.filter(214885),  // Croatia
+            g.filter(307787),  // Cyprus
+            g.filter(51684),   // Czechia
+            g.filter(50046),   // Denmark
+            g.filter(79510),   // Estonia
+            g.filter(54224),   // Finland
+            g.filter(2202162), // France
+            g.filter(51477),   // Germany
+            g.filter(192307),  // Greece
+            g.filter(62273),   // Ireland
+            g.filter(365331),  // Italy
+            g.filter(72594),   // Latvia
+            g.filter(72596),   // Lithuania
+            g.filter(2171347), // Luxembourg
+            g.filter(365307),  // Malta
+            g.filter(47796),   // Netherlands
+            g.filter(21335),   // Hungary
+            g.filter(49715),   // Poland
+            g.filter(295480),  // Portugal
+            g.filter(1311341), // Spain
+            g.filter(218657),  // Slovenia
+            g.filter(14296),   // Slovakia
+            g.filter(52822),   // Sweden
+            g.filter(90689),   // Romania
+        ],
+        FilePointWriter::new("roads-eu.dat"),
+    ));
 }
